38b181d7693189fd233879c1dc6755234c51f94c
Kyle Corry <kylecorry31@gmail.com>
2020-03-07 11:44:01 -0500

    Give plant a default pressure range

--- desk_plant/desk_plant.ino
+++ desk_plant/desk_plant.ino
@@ -8,7 +8,10 @@ Adafruit_BME280 barometer{};
 TriColorLED led{5, 3, 2};
 LightSensor light_sensor{A0};
 
-#define ADAPTIVE_PRESSURE true
+const long adapted_threshold = 800L; // 8 hPa
+
+long default_min_pressure = 99500L; // 995 hPa
+long default_max_pressure = 101800L; // 1018 hPa
 
 long min_pressure;
 long max_pressure;
@@ -17,24 +20,28 @@ void setup() {
   while(!barometer.begin(0x76));
 
   long pressure = GetPressure();
-  if (ADAPTIVE_PRESSURE){
-    min_pressure = pressure - 100L;
-    max_pressure = pressure + 100L;
-  } else {
-    min_pressure = 99000L;
-    max_pressure = 101000L;  
-  }
+  min_pressure = pressure;
+  max_pressure = pressure;
 }
 
 void loop() {
   long pressure = GetPressure();
   min_pressure = min(pressure, min_pressure);
   max_pressure = max(pressure, max_pressure);
+
+  long actual_min_pressure = min(min_pressure, default_min_pressure);
+  long actual_max_pressure = max(max_pressure, default_max_pressure);
+
+  if (max_pressure - min_pressure >= adapted_threshold){
+    // Plant has adapted to environment
+    actual_min_pressure = min_pressure;
+    actual_max_pressure = max_pressure;  
+  }
   
   if (light_sensor.IsDark()){
     led.Off();  
   } else {
-    int pressure_amount = (int)map(pressure, min_pressure, max_pressure, 0, 255);
+    int pressure_amount = (int)map(pressure, actual_min_pressure, actual_max_pressure, 0, 255);
     SetColor(pressure_amount);
   }
 }
